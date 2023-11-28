9c9e48bb1f8ba35ab6b63be620c5471b32091ea3
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-28 07:58:23 -0500

    Create greeting path in App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greeting from './Greeting';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
