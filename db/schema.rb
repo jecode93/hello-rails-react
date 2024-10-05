4e2ae0486c8d98495e06482ab907fa442a48fdb2
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-10-05 19:41:36 -0400

    File modify after running rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_103413) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_05_231108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,5 +25,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_103413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
